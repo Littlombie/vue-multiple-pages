e279229a1f64bf16b7c814910dfb8c1e4d38bd73
Littlombie <Littlombie@users.noreply.github.com>
2018-11-26 10:53:47 +0800

    Configuration of multi-page routing

--- src/pages/about/router.js
+++ src/pages/about/router.js
@@ -1,16 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import About from './views/About.vue'
+import aboutYou from './views/aboutyou.vue'
 
 Vue.use(Router)
 
-export default new Router({
+const VueRouter = new Router ({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
     {
       path: '/about',
       name: 'about',
+      meta: {
+        title: 'About'
+      },
       component: About
     },
     {
@@ -19,10 +23,25 @@ export default new Router({
       component: About
     },
     {
-      path: '/home',
-      beforeEnter(){
-        window.location.href = 'index.html'
-      }
+      path: '/aboutYou',
+      name:'aboutYou',
+      meta: {
+        title: 'About You'
+      },
+      component: aboutYou
     }
   ]
 })
+VueRouter.beforeEach((to, from, next) => {
+  if (to.meta.title) {
+    document.title = to.meta.title
+  }
+  next()
+})
+
+VueRouter.afterEach(() => {
+  // 跳到每个页面的时候 让页面滑动到最顶端
+  window.scrollTo(0, 0)
+})
+
+export default VueRouter
